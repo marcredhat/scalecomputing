3774ce9c4b43075e7d6e1baaf61ffa02209d735d
Marc Chisinevski <marcgcp6@gmail.com>
2022-11-06 15:26:19 -0800

    Update preplab1.sh

--- preplab1.sh
+++ preplab1.sh
@@ -31,3 +31,27 @@ mkdir /local-storage
 mount /dev/vdb1 /docker-storage
 mount /dev/vdc1 /longhorn-storage
 mount /dev/vdd1 /local-storage
+
+
+#remove firewalld and flush iptables on all machines
+yum -y remove firewalld
+# Accept all traffic first to avoid ssh lockdown  via iptables firewall rules #
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+ 
+# Flush All Iptables Chains/Firewall rules #
+iptables -F
+ 
+# Delete all Iptables Chains #
+iptables -X
+ 
+# Flush all counters too #
+iptables -Z 
+# Flush and delete all nat and  mangle #
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
