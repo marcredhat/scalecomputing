7af0e289b6cdd47b5a381b5d5569565d8cc79b5b
Marc Chisinevski <marcgcp6@gmail.com>
2022-11-10 11:10:47 -0800

    Update preplab1.sh

--- preplab1.sh
+++ preplab1.sh
@@ -25,6 +25,8 @@ mount /dev/vdb1 /data/data1
 mount /dev/vdc1 /data/data2
 mount /dev/vdd1 /data/data3
 
+
+
 #For ECS:
 mkdir /docker-storage
 mkdir /longhorn-storage
@@ -57,3 +59,24 @@ iptables -t mangle -F
 iptables -t mangle -X
 iptables -t raw -F
 iptables -t raw -X
+
+
+
+#
+# /etc/fstab
+# Created by anaconda on Thu Nov 10 13:02:18 2022
+#
+# Accessible filesystems, by reference, are maintained under '/dev/disk'
+# See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
+#
+/dev/mapper/centos-root /                       xfs     defaults        0 0
+UUID=e93c9d87-544d-4541-9b1b-cef94fb7831d /boot                   xfs     defaults        0 0
+UUID=CAA6-6FA3          /boot/efi               vfat    umask=0077,shortname=winnt 0 0
+#/dev/mapper/centos-home /home                   xfs     defaults        0 0
+/dev/mapper/centos-swap swap                    swap    defaults        0 0
+/dev/vda1 /docker-storage                       xfs    defaults        0 0
+/dev/vdc1 /longhorn-storage                       xfs    defaults        0 0
+/dev/vdd1 /local-storage                       xfs    defaults        0 0
+
+
+Test with mount -a
